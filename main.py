69e006aff53ba5042590c8299d68f6aad907398b
Kirill <kirillfr97@mail.ru>
2024-10-16 15:39:41 +0500

    lesson 3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from enum import Enum
+
 from fastapi import FastAPI
-from sqlalchemy.testing.pickleable import User
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Trading App"
@@ -10,11 +13,32 @@ app = FastAPI(
 fake_users = [
     {"id": 1, "username": "Bob", "role": "admin"},
     {"id": 2, "username": "Claire", "role": "investor"},
-    {"id": 3, "username": "Smith", "role": "trader"}
+    {"id": 3, "username": "Smith", "role": "trader"},
+    {"id": 4, "username": "Homer", "role": "investor", "degree": [
+        {"id": 1, "created_at": "2020-05-21T00:00:00", "type": "expert"}
+    ]}
 ]
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: list[Degree] | None = []
+
+
+@app.get("/users/{user_id}", response_model=list[User])
 def get_user(user_id):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -35,3 +59,19 @@ def change_name(user_id: int, new_name: str):
     current_user = filter(lambda user: user.get("id") == user_id, fake_users)
     current_user["name"] = new_name
     return {"status": "success", "data": current_user}
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str
+    amount: float
+    price: float = Field(ge=0, example=123.45)
+    side: str
+
+
+@app.post("/trades")
+def add_trade(trades: list[Trade]):
+    fake_trades.extend(trades)
+    return {"status": "success", "data": fake_trades}
+
