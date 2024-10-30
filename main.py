fe98845ac4d7b05f0affd29b94933a324cd12abf
Kirill <kirillfr97@mail.ru>
2024-10-30 22:53:24 +0500

    lesson 5 + fixes

--- main.py
+++ main.py
@@ -1,76 +1,34 @@
-from datetime import datetime
-from enum import Enum
-
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi_users import FastAPIUsers
+
+from auth.database import User
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI(
     title="Trading App"
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-# Fake BD
-fake_users = [
-    {"id": 1, "username": "Bob", "role": "admin"},
-    {"id": 2, "username": "Claire", "role": "investor"},
-    {"id": 3, "username": "Smith", "role": "trader"},
-    {"id": 4, "username": "Homer", "role": "investor", "degree": [
-        {"id": 1, "created_at": "2020-05-21T00:00:00", "type": "expert"}
-    ]}
-]
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-
-class DegreeType(Enum):
-    newbie = "newbie"
-    expert = "expert"
-
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: list[Degree] | None = []
-
-
-@app.get("/users/{user_id}", response_model=list[User])
-def get_user(user_id):
-    return [user for user in fake_users if user.get("id") == user_id]
-
-
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "USD", "amount": 2.15, "price": 123, "side": "sell"},
-    {"id": 2, "user_id": 1, "currency": "USD", "amount": 2.15, "price": 125.6, "side": "buy"},
-]
-
-
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
-
-
-@app.post("users/{user_id}")
-def change_name(user_id: int, new_name: str):
-    current_user = filter(lambda user: user.get("id") == user_id, fake_users)
-    current_user["name"] = new_name
-    return {"status": "success", "data": current_user}
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str
-    amount: float
-    price: float = Field(ge=0, example=123.45)
-    side: str
-
-
-@app.post("/trades")
-def add_trade(trades: list[Trade]):
-    fake_trades.extend(trades)
-    return {"status": "success", "data": fake_trades}
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
--- models/models.py
+++ models/models.py
@@ -1,12 +1,12 @@
 from datetime import datetime, UTC
 
-from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON
+from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON, Boolean
 
 # Специальная переменная
 metadata = MetaData()
 
-roles = Table(
-    'roles',
+role = Table(
+    'role',
     metadata,
     Column("id", Integer, primary_key=True),
     # nullable говорит о том, что этот параметр не может быть пустой
@@ -14,17 +14,20 @@ roles = Table(
     Column("permissions", JSON),
 )
 
-users = Table(
-    'users',
+user = Table(
+    'user',
     metadata,
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
-    Column("registered_on", TIMESTAMP, nullable=False, default=datetime.now(UTC)),
+    Column("hashed_password", String, nullable=False),
+    Column("registered_on", TIMESTAMP(timezone=True), nullable=False, default=datetime.now(UTC)),
     # ForeignKey как ссылка на другую таблицу
     # + более понятная логика связи таблиц
     # + нельзя использовать не существующие данные в таблицу (консистентность данных)
     # + нельзя просто так удалить таблицу, так как между ними есть связь
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False)
 )
